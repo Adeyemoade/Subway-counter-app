a25fcfc673051e6f39e59154844dd352c474015c
Adeyemoade <127961998+Adeyemoade@users.noreply.github.com>
2023-06-04 10:36:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+function save() {
+    let countsave = count + " - "
+    saveEl.textContent += countsave
+    countEl.textContent = 0
+    count = 0
+}
